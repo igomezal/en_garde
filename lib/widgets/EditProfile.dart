16ada9894109a19ec121a29005ccb796869ef9a9
Iván Gómez Alonso <yvangomezalonso@gmail.com>
2020-09-24 01:21:49 +0200

    fix: wait to get the data and then render widget

--- lib/widgets/EditProfile.dart
+++ lib/widgets/EditProfile.dart
@@ -15,93 +15,98 @@ class EditProfile extends StatefulWidget {
 class _EditProfile extends State<EditProfile> {
   bool _notValid = false;
   String _telephone = '';
+  Map<String, int> _regionMapNumbers;
 
   @override
   initState() {
     super.initState();
-    _telephone = Provider.of<UserFromFireStore>(context, listen: false).telephone;
+    _telephone =
+        Provider.of<UserFromFireStore>(context, listen: false).telephone;
+    PhoneNumber().allSupportedRegions().then((value) => setState(() {
+          _regionMapNumbers = value;
+        }));
   }
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<Map<String, int>>(
-      future: PhoneNumber().allSupportedRegions(),
-      builder: (context, regionMapNumbers) {
-        return Consumer2<User, UserFromFireStore>(
-            builder: (context, user, userFromFireStore, child) {
-              return Card(
-            child: Column(children: [
-              const ListTile(
-                title: Text(
-                  'Edit your telephone number:',
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                ),
-              ),
-              ListTile(
-                leading: Icon(
-                  Icons.person,
-                  color: Colors.blue,
-                ),
-                title: Text(user?.displayName ?? ''),
-              ),
-              ListTile(
-                leading: Icon(Icons.mail, color: Colors.blue),
-                title: Text(user?.email ?? ''),
-              ),
-              Container(
-                padding: EdgeInsets.all(8),
-                child: FutureProvider<Map<String, dynamic>>(
-                  create: (_) =>
-                      PhoneNumber().parse(userFromFireStore.telephone),
-                  builder: (context, child) {
-                    return Consumer<Map<String, dynamic>>(
-                        builder: (context, phone, child) {
-                      return IntlPhoneField(
-                        initialValue: phone != null ? phone['national_number'] : '',
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: 'Telephone',
-                          errorText: _notValid
-                              ? 'Provide a valid telephone number.'
-                              : null,
-                        ),
-                        onChanged: (telephone) async {
-                          _telephone = telephone.completeNumber;
-                          try {
-                            await PhoneNumber().parse(telephone.completeNumber);
-                            _changeNotValid(false);
-                          } on PlatformException catch (e) {
-                            _changeNotValid(true);
-                            print(e);
-                          }
-                        },
-                        onSubmitted: (_) {
-                          _submitTelephone(user.uid, _telephone);
-                        },
-                        initialCountryCode: getRegionCode(
-                            regionMapNumbers.data, phone != null ? phone['country_code'] : '34'),
-                      );
-                    });
-                  },
-                ),
-              ),
-              ButtonBar(
-                children: [
-                  RaisedButton(
-                    color: Colors.blue,
-                    textColor: Colors.white,
-                    child: const Text('SUBMIT'),
-                    onPressed: () {
-                      _submitTelephone(user.uid, _telephone);
-                    },
-                  )
-                ],
+    return Consumer2<User, UserFromFireStore>(
+        builder: (context, user, userFromFireStore, child) {
+      return Card(
+        child: Column(children: [
+          const ListTile(
+            title: Text(
+              'Edit your telephone number:',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.person,
+              color: Colors.blue,
+            ),
+            title: Text(user?.displayName ?? ''),
+          ),
+          ListTile(
+            leading: Icon(Icons.mail, color: Colors.blue),
+            title: Text(user?.email ?? ''),
+          ),
+          Container(
+            padding: EdgeInsets.all(8),
+            child: FutureProvider<Map<String, dynamic>>(
+              create: (_) => PhoneNumber().parse(userFromFireStore.telephone),
+              builder: (context, child) {
+                return Consumer<Map<String, dynamic>>(
+                    builder: (context, phone, child) {
+                  // Improvement: Show skeleton or any other kind of loader in the EditProfile space to avoid visual glitches
+                  return userFromFireStore?.telephone != null &&
+                          _regionMapNumbers != null
+                      ? IntlPhoneField(
+                          initialValue:
+                              phone != null ? phone['national_number'] : '',
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Telephone',
+                            errorText: _notValid
+                                ? 'Provide a valid telephone number.'
+                                : null,
+                          ),
+                          onChanged: (telephone) async {
+                            _telephone = telephone.completeNumber;
+                            try {
+                              await PhoneNumber()
+                                  .parse(telephone.completeNumber);
+                              _changeNotValid(false);
+                            } on PlatformException catch (e) {
+                              _changeNotValid(true);
+                              print(e);
+                            }
+                          },
+                          onSubmitted: (_) {
+                            _submitTelephone(user.uid, _telephone);
+                          },
+                          initialCountryCode: getRegionCode(_regionMapNumbers,
+                              phone != null ? phone['country_code'] : '34'),
+                        )
+                      : Container();
+                });
+              },
+            ),
+          ),
+          ButtonBar(
+            children: [
+              RaisedButton(
+                color: Colors.blue,
+                textColor: Colors.white,
+                child: const Text('SUBMIT'),
+                onPressed: () {
+                  _submitTelephone(user.uid, _telephone);
+                },
               )
-            ]),
-          );
-        });
-      },
-    );
+            ],
+          )
+        ]),
+      );
+    });
   }
 
   String getRegionCode(Map<String, int> regionMapNumbers, String regionNumber) {
@@ -117,8 +122,9 @@ class _EditProfile extends State<EditProfile> {
   }
 
   void _submitTelephone(String userId, String telephone) {
-    if(!_notValid) {
-      Provider.of<DatabaseService>(context, listen: false).changeTelephone(userId, telephone);
+    if (!_notValid) {
+      Provider.of<DatabaseService>(context, listen: false)
+          .changeTelephone(userId, telephone);
       FocusScope.of(context).unfocus();
 
       final snackBar = SnackBar(content: Text('Telephone updated correctly!'));
