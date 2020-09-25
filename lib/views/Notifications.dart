2f99f3d8a90735bb62fb684cd7f0882ee5beede9
Iván Gómez Alonso <yvangomezalonso@gmail.com>
2020-09-25 10:59:16 +0200

    fix: expand listview to fill the layout

--- lib/views/Notifications.dart
+++ lib/views/Notifications.dart
@@ -3,14 +3,15 @@ import 'package:flutter/material.dart';
 class Notifications extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-        itemCount: 255,
-        itemBuilder: (BuildContext context, int index) {
-          return ListTile(
-            leading: Icon(Icons.fiber_new),
-            trailing: Icon(Icons.delete),
-            title: Text('Notification'),
-          );
-        });
+    return Expanded(
+        child: ListView.builder(
+            itemCount: 255,
+            itemBuilder: (BuildContext context, int index) {
+              return ListTile(
+                leading: Icon(Icons.fiber_new),
+                trailing: Icon(Icons.delete),
+                title: Text('Notification'),
+              );
+            }));
   }
 }
